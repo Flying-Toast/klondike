01f51f53ae1c9d5c26fb55d35157e8e4f3f4a5fe
Flying-Toast <38232168+Flying-Toast@users.noreply.github.com>
2019-11-03 21:30:17 -0500

    struct Waste

--- src/components.rs
+++ src/components.rs
@@ -40,12 +40,39 @@ impl Pile for Stock {
         &self.cards
     }
 
-    fn can_push(&self, card: &Card) -> bool {
+    fn can_push(&self, _card: &Card) -> bool {
         //can't ever push onto the stock
         false
     }
 
-    fn push(&mut self, card: Card) {
+    fn push(&mut self, _card: Card) {
         panic!("Can't push onto stock.");
+    }
+}
+
+pub struct Waste {
+    cards: Vec<Card>,
+}
+
+impl Waste {
+    pub fn new() -> Self {
+        Self {
+            cards: vec![]
+        }
+    }
+}
+
+impl Pile for Waste {
+    fn cards(&self) -> &[Card] {
+        &self.cards
+    }
+
+    fn can_push(&self, _card: &Card) -> bool {
+        //can always push onto waste
+        true
+    }
+
+    fn push(&mut self, card: Card) {
+        self.cards.insert(0, card);
     }
 }
